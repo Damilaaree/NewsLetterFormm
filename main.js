646c9a00bf4bfb5a4e2106edd9d0f15ad8e1f7f4
Damilare <121191782+Damilaaree@users.noreply.github.com>
2023-07-20 22:56:41 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,43 +1,8 @@
-const containerE2 = document.getElementById('container')
-const leftE2 = document.getElementById('left')
-const rightE2 = document.getElementById('right')
-
-const userEmailE1 = document.getElementById('user-email')
-const invalidEmailE1 = document.getElementById('invalid-email')
-const emailInput = document.getElementById('email')
-const submitBtnE1 = document.getElementById('submit-btn')
-
-const confirmedMessageE1 = document.getElementById('confirmed-message')
-const dismissMessageE1 = document.getElementById('dismiss-message')
-
-function formSuccess(){
-    confirmedMessageE1.classList.add('active')
-    containerE2.classList.add('success')
-    leftE2.style.display='none'
-    rightE2.style.display='none'
-    }
-    function validateEmail(email){
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    return emailRegex.test(email)
-    }
-    submitBtnE1.addEventListener('click',(e) => {
-    e.preventDefault()
-    const email=emailInput.value.trim()
-
-    if(validateEmail(email)){
-    formSuccess()
-    userEmailE1.innerText = email
-    emailInput.value = ''
-    invalidEmailE1.classList.remove('active')
-    emailInput.classList.remove('active')
-    }else{
-    invalidEmailE1.classList.add('active')
-    emailInput.classList.add('active')
-    }
-    })
-    dismissMessageE1.addEventListener('click',() =>{
-    leftE2.style.display = 'block'
-    rightE2.style.display='block'
-    containerE2.classList.remove('success')
-    confirmedMessageE1.classList,remove('active')
-    })
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card");
+const card_2 = document.querySelector(".card-2");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
